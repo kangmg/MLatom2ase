753806ee75cceac96e5a5ae0c15e8dee550ad897
Mingi Kang <59556369+kangmg@users.noreply.github.com>
2024-11-22 06:18:45 +0000

    convert submodule to regular folder

---
+++ mlatom2ase/__init__.py
@@ -0,0 +1,23 @@
+__version__ = '0.0.1'
+
+# ---------- check $dft4bin ---------- #
+import subprocess
+import warnings
+
+def dft4binChecker()->:
+  """check if dft4 is installed
+  """
+  try:
+    return True if subprocess.run(['dft4', '--version']).returncode == 0 else False
+  except FileNotFoundError:
+    return False
+
+# ------------------------------------ #
+
+if dft4binChecker():
+    from .ase_interface import MLatomCalculator
+else:
+    warnings.warn(
+        '$dft4bin is not found.\nPlease set the environment variable: export dftd4bin=/path/to/dft4bin',
+        category=ImportWarning
+    )
---
+++ mlatom2ase/ase_interface.py
@@ -0,0 +1,88 @@
+
+import numpy as np
+import mlatom as ml
+import ase
+from ase.calculators.calculator import Calculator, all_changes
+from ase.units import Hartree
+
+class MLatom2Calculator(Calculator):
+    """
+    ASE Calculator for AIQM & AIQM2 model using MLAtom
+
+    Implements energy and force calculations with proper system change handling
+    """
+    implemented_properties = ['energy', 'forces']
+
+    def __init__(self, charge=0, multiplicity=1, method='AIQM2'):
+        """
+        Initialize the AIQM2 Calculator
+
+        Parameters:
+        -----------
+        charge : int, optional
+            Molecular system charge (default: 0)
+        multiplicity : int, optional
+            Spin multiplicity (default: 1)
+        method : str, optional
+            mlatom method (default: 'AIQM2')
+
+        Note)
+        'aiqm2': ['AIQM2', 'AIQM2@DFT']
+        'torchani': [
+          "ANI-1x", "ANI-1ccx", "ANI-2x", 'ANI-1x-D4',
+          'ANI-2x-D4', 'ANI-1xnr', 'ANI-1ccx-gelu',
+          'ANI-1ccx-gelu-D4', 'ANI-1x-gelu', 'ANI-1x-gelu-d4'
+          ]
+        """
+        super().__init__()
+        self.charge = charge
+        self.multiplicity = multiplicity
+        self._model = ml.models.methods(method=method)
+        self.molecule = None
+
+    def _convert_mlatom_molecule(self, atoms:ase.Atoms):
+        """Convert ASE Atoms to MLAtom molecule"""
+
+        molecule = ml.data.molecule(
+            charge=self.charge,
+            multiplicity=self.multiplicity
+        ).read_from_numpy(
+            coordinates=self.atoms.positions,
+            species=np.array(self.atoms.symbols)
+        )
+        self.molecule = molecule
+
+    def calculate(self, atoms=None, properties=None, system_changes=all_changes):
+        """
+        Calculate energy and forces for the system
+
+        Parameters:
+        -----------
+        atoms : ase.Atoms
+            Atoms object to calculate
+        properties : list
+            Properties to calculate
+        system_changes : list
+            List of system changes
+        """
+        # Apply standard Calculator checks for system changes
+        if properties is None:
+            properties = self.implemented_properties
+
+        super().calculate(atoms, properties, system_changes)
+
+        # mlatom molecule object
+        self._convert_mlatom_molecule(atoms)
+
+        # calculate energy and gradients
+        self._model.predict(
+            molecule=self.molecule,
+            calculate_energy=True if 'energy' in properties else False,
+            calculate_energy_gradients=True if 'forces' in properties else False
+        )
+
+        # unit conversion
+        # energy : Ha => eV
+        # forces : Ha/Ang => eV/Ang
+        self.results['energy'] = self.molecule.energy * Hartree
+        self.results['forces'] = - self.molecule.energy_gradients * Hartree
