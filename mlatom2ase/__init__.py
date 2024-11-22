d5166c8cba81917a05dddf52735c1df18866502d
Mingi Kang <59556369+kangmg@users.noreply.github.com>
2024-11-22 06:25:00 +0000

    [fix] module is not imported

--- mlatom2ase/__init__.py
+++ mlatom2ase/__init__.py
@@ -3,6 +3,7 @@ __version__ = '0.0.1'
 # ---------- check $dft4bin ---------- #
 import subprocess
 import warnings
+from .ase_interface import MLatomCalculator
 
 def dft4binChecker():
   """check if dft4 is installed
@@ -14,9 +15,7 @@ def dft4binChecker():
 
 # ------------------------------------ #
 
-if dft4binChecker():
-    from .ase_interface import MLatomCalculator
-else:
+if not dft4binChecker():
     warnings.warn(
         '$dft4bin is not found.\nPlease set the environment variable: export dftd4bin=/path/to/dft4bin',
         category=ImportWarning
